ec07a7bae6f773acf6651f4dc4b78514a896cfba
ixpkgs <ixpkgs@140.lol>
2026-09-18 19:36:25 +0900

    Package codex rust-v0.92.0
    Upstream commit: a09055074e082d70e8b92795b0cec1e969a6aaf9

--- flake.nix
+++ flake.nix
@@ -20,9 +20,9 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
           lib = pkgs.lib;
-          tag = "rust-v0.91.0";
-          version = "rust-v0.91.0";
-          commit = "3684bc646e0f1123e76a90f03cfb1111358f1aa1";
+          tag = "rust-v0.92.0";
+          version = "rust-v0.92.0";
+          commit = "a09055074e082d70e8b92795b0cec1e969a6aaf9";
           upstreamVersion = lib.removePrefix "rust-v" version;
           useGnuLinux =
             lib.versionAtLeast upstreamVersion "0.54.0"
@@ -34,10 +34,10 @@
             aarch64-darwin = "aarch64-apple-darwin";
           };
           assetHashes = {
-            x86_64-linux = "sha256-OUTyltEAOUvqS4TLoHwROuGeEvcoStQ8h9sb/8UCnho=";
-            aarch64-linux = "sha256-T/4M4Rw0Lpb8h4BGD9ghbIj5iKmfTXi7yZEGqwAf4fc=";
-            x86_64-darwin = "sha256-sSr+bsL/AFiV46szHGfsr9P9D8N00GmL1WmPukK32Xc=";
-            aarch64-darwin = "sha256-sWBtHwQ9bIfMkyblnSazE1PNgk0vp+n0ZeTxL69HWxg=";
+            x86_64-linux = "sha256-Mgyy1BaAgpVOLNcCLNOMncqi1gdE1H6c0A0Oa4rm+zw=";
+            aarch64-linux = "sha256-1335wRuetrdc0oMEV2+gbx7Fl9A+hQvvDST5EQRgFmQ=";
+            x86_64-darwin = "sha256-er78jjbfdDx6z+UkYpPj/S4pWLE3yXdvl2rtjjqRyek=";
+            aarch64-darwin = "sha256-GgY+04e7Be8LKHWpZBdpcwiXXtZX4BlXi8R410weKIk=";
           };
           target = targets.${system};
           isBundle = lib.versionAtLeast upstreamVersion "0.140.0";
